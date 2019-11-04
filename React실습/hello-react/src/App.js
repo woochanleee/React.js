8a1cf09bcdcb09618688ac13c9699a709e911fea
woochanleee <030219woo@naver.com>
2019-11-04 22:22:58 +0900

    props

--- React실습/hello-react/src/App.js
+++ React실습/hello-react/src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Counter from './Counter';
 
-class App extends Component {
-  render() {
-    const name = 'react';
-    return <div className="react">{name}</div>;
-  }
-}
+const App = () => {
+  return <Counter />
+};
 
 export default App;
---
+++ React실습/hello-react/src/Counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
+    render() {
+        const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
+                <button
+                    // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
+                    onClick={() => {
+                        // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
+                        this.setState(
+                            {
+                                number: number + 1
+                            },
+                            () => {
+                                console.log('방금 setState가 호출되었습니다.');
+                                console.log(this.state);
+                            }
+                        );
+                    }}
+                >
+                    +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
