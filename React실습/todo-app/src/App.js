25d8f2e0b5d561c7a423007613a90d22b4d18a1e
woochanleee <030219woo@naver.com>
2019-11-13 23:22:16 +0900

    Finish styling

--- React실습/todo-app/src/App.js
+++ React실습/todo-app/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <div>Todo App을 만들자!</div>
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  );
 };
 
 export default App;
