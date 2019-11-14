88e0d687bbb718574e80f49872738678f447942a
woochanleee <030219woo@naver.com>
2019-11-14 16:21:32 +0900

    Finish Todo-List project

--- React실습/todo-app/src/components/TodoList.js
+++ React실습/todo-app/src/components/TodoList.js
@@ -2,12 +2,17 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
     return (
         <div className="TodoList">
-            <TodoListItem />
-            <TodoListItem />
-            <TodoListItem />
+            {todos.map(todo => (
+                <TodoListItem 
+                    todo={todo} 
+                    key={todo.id} 
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
+            ))}
         </div>
     );
 };
--- React실습/todo-app/src/components/TodoListItem.js
+++ React실습/todo-app/src/components/TodoListItem.js
@@ -4,17 +4,19 @@ import {
     MdCheckBox,
     MdRemoveCircleOutline,
 } from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+    const { id, text, checked } = todo;
     return (
         <div className="TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank />
-                <div className="text">할일</div>
+            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
             </div>
             <div className="remove">
-                <MdRemoveCircleOutline />
+                <MdRemoveCircleOutline onClick={() => onRemove(id)} />
             </div>
         </div>
     );
