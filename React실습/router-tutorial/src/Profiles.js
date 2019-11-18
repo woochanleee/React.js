7725ad665954cf04a4eec507a887b26005ea93e6
woochanleee <030219woo@naver.com>
2019-11-18 22:28:31 +0900

    Develop spa with a reactive router

--- React실습/router-tutorial/src/Profiles.js
+++ React실습/router-tutorial/src/Profiles.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 
 const Profiles = () => {
+  const activeStyle = {
+    background: 'black',
+    color: 'white'
+  };
     return (
       <div>
         <h3>사용자 목록:</h3>
         <ul>
           <li>
-            <Link to="/profiles/velopert">velopert</Link>
+            <NavLink activeStyle={activeStyle} to="/profiles/velopert" active>velopert</NavLink>
           </li>
           <li>
-            <Link to="/profiles/gildong">gildong</Link>
+            <NavLink activeStyle={activeStyle} to="/profiles/gildong">gildong</NavLink>
           </li>
         </ul>
       
