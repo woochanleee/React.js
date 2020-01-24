5a69cf9ed92296001cac824ced0185ac8aa157a8
leewoochan <030219woo@naver.com>
2020-01-24 19:00:53 +0900

    update: initialstate and reducer function

--- React실습/react-redux-tutorial/src/modules/counter.js
+++ React실습/react-redux-tutorial/src/modules/counter.js
@@ -8,3 +8,19 @@ const initialState = {
     number: 0
 };
 
+function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREASE:
+            return {
+                number: state.number + 1
+            };
+        case DECREASE:
+            return {
+                number: state.number - 1
+            };
+        default:
+            return state;
+    }
+}
+
+export default counter;
