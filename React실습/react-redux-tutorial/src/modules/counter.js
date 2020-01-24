df8f9416f7ab6e3e6de0298a5a63424055e39020
leewoochan <030219woo@naver.com>
2020-01-24 19:00:25 +0900

    update: action create function

--- React실습/react-redux-tutorial/src/modules/counter.js
+++ React실습/react-redux-tutorial/src/modules/counter.js
@@ -1,3 +1,10 @@
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+    number: 0
+};
+
