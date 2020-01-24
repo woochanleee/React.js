9e66f03606c0f7918c8b6e2877d3a79a41945dae
leewoochan <030219woo@naver.com>
2020-01-24 17:49:39 +0900

    add: counter UI

---
+++ React실습/react-redux-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+const App = () => {
+  return (
+    <div>
+      <Counter number={0} />
+    </div>
+  );
+};
+
+export default App;
--- React실습/react-redux-tutorial/src/components/Counter.js
+++ React실습/react-redux-tutorial/src/components/Counter.js
@@ -5,8 +5,8 @@ const Counter = ({ number, onIncrease, onDecrease }) => {
         <div>
             <h1>{number}</h1>
             <div>
-                <button onClick={onIncrease} >+1</button>
-                <button onClick={onDecrease} >-1</button>
+                <button onClick={onIncrease}>+1</button>
+                <button onClick={onDecrease}>-1</button>
             </div>
         </div>
     );
