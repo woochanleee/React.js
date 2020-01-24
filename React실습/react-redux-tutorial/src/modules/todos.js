f4c9cb09773fb94f7106086567b9a26ee5f9af7d
leewoochan <030219woo@naver.com>
2020-01-24 19:01:20 +0900

    add: todos module

---
+++ React실습/react-redux-tutorial/src/modules/todos.js
@@ -0,0 +1,5 @@
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
+const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
+const TOGGLE = 'todos/TOGGLE'; // todos를 체크/체크 해제함
+const REMOVE = 'todos/REMOVE'; // todo를 제거함
+
