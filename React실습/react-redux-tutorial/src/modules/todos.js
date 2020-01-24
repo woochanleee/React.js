252e7f8b1f156c31d166caa1267af607114d1f05
leewoochan <030219woo@naver.com>
2020-01-24 19:24:01 +0900

    update: reducer

--- React실습/react-redux-tutorial/src/modules/todos.js
+++ React실습/react-redux-tutorial/src/modules/todos.js
@@ -28,3 +28,49 @@ export const remove = id => ({
     id
 });
 
+const initialState = {
+    input: '',
+    todos: [
+        {
+            id: 1,
+            text: '리덕스 기초 배우기',
+            done: true
+        },
+        {
+            id: 2,
+            text: '리액트와 리덕스 사용하기',
+            done: false
+        },
+    ],
+};
+
+function todos(state = initialState, action) {
+    switch (action.type) {
+        case CHANGE_INPUT:
+            return {
+                ...state,
+                input: action.input
+            };
+        case INSERT:
+            return {
+                ...state,
+                todos: state.todos.concat(action.todo)
+            };
+        case TOGGLE:
+            return {
+                ...state,
+                todos: state.todos.map(todo =>
+                    todo.id === action.id ? { ...todo, done: !todo.done } : todo    
+                )
+            };
+        case REMOVE:
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.id !== action.id)
+            };
+        default:
+            return state;
+    }
+}
+
+export default todos;
