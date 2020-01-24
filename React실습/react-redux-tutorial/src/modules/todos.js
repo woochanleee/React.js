697ad0308af4659e0b265779fbc940923302f360
leewoochan <030219woo@naver.com>
2020-01-24 19:02:08 +0900

    update: action create function

--- React실습/react-redux-tutorial/src/modules/todos.js
+++ React실습/react-redux-tutorial/src/modules/todos.js
@@ -3,3 +3,28 @@ const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
 const TOGGLE = 'todos/TOGGLE'; // todos를 체크/체크 해제함
 const REMOVE = 'todos/REMOVE'; // todo를 제거함
 
+export const changeInput = input => ({
+    type: CHANGE_INPUT,
+    input
+});
+
+let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
+export const insert = text => ({
+    type: INSERT,
+    todo: {
+        id: id++,
+        text,
+        done: false
+    }
+});
+
+export const toggle = id => ({
+    type: TOGGLE,
+    id
+});
+
+export const remove = id => ({
+    type: REMOVE,
+    id
+});
+
