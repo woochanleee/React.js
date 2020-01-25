eb2f99b062ee63eba65c8faa0873d24931101098
leewoochan <030219woo@naver.com>
2020-01-25 14:48:37 +0900

    update: using useActions

--- React실습/react-redux-tutorial/src/lib/useActions.js
+++ React실습/react-redux-tutorial/src/lib/useActions.js
@@ -2,7 +2,7 @@ import { bindActionCreators } from 'redux'
 import { useDispatch } from 'react-redux'
 import { useMemo } from 'react'
 
-export function useActions(actions, deps) {
+export default function useActions(actions, deps) {
   const dispatch = useDispatch()
   return useMemo(
     () => {
