ce8c636da95b03f24c96b9567640c8ab60366528
leewoochan <030219woo@naver.com>
2020-01-25 14:24:46 +0900

    update: useSelector

--- React실습/react-redux-tutorial/src/containers/CounterContainer.js
+++ React실습/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter'
 
-const CounterContainer = ({ number, increase, decrease }) => {
-    return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    return <Counter number={number} />;
 };
-/*
-const mapStateToPops = state => ({
-    number: state.counter.number,
-});
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease: () => {
-        dispatch(decrease());
-    }
-});
 
-export default connect(
-    mapStateToPops,
-    mapDispatchToProps,
-)(CounterContainer);
-
-*/
-export default connect(
-    state => ({
-        number: state.counter.number
-    }),
-    {
-        increase,
-        decrease,
-    }
-)(CounterContainer);
+export default CounterContainer;
