8d5151657cc8e4c95cddc3e8ef9268709c588708
leewoochan <030219woo@naver.com>
2020-01-26 23:54:00 +0900

    add: Counter container

---
+++ React실습/learn-redux-middleware/src/containers/CounterContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
+import Counter from '../components/Counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+    return (
+        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    );
+};
+
+export default connect(
+    state => ({
+        number: state.counter
+    }),
+    {
+        increase,
+        decrease
+    }
+)(CounterContainer);
