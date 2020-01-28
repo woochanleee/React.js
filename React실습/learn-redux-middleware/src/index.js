886132edd366b80f5d05258024d7e6707d010ec9
leewoochan <030219woo@naver.com>
2020-01-28 23:12:34 +0900

    update: apply middleware

--- React실습/learn-redux-middleware/src/index.js
+++ React실습/learn-redux-middleware/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
     <Provider store={store}>
