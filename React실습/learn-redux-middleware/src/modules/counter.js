24adcea372ff3a76104fdd50bcf7ac34cfeedf61
leewoochan <030219woo@naver.com>
2020-01-28 21:55:53 +0900

    modify: fix mistake

--- React실습/learn-redux-middleware/src/modules/counter.js
+++ React실습/learn-redux-middleware/src/modules/counter.js
@@ -1,4 +1,4 @@
-import { createAction, handleAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -8,7 +8,7 @@ export const decrease = createAction(DECREASE);
 
 const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
 
-const counter = handleAction(
+const counter = handleActions(
     {
         [INCREASE]: state => state + 1,
         [DECREASE]: state => state - 1
