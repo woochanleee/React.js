7a3f0595105007f47c0276e4a983c3650a4f1ef8
leewoochan <030219woo@naver.com>
2020-01-29 23:33:35 +0900

    add: sample reducer

---
+++ React실습/learn-redux-middleware/src/modules/sample.js
@@ -0,0 +1,90 @@
+import { handleActions } from 'redux-actions';
+import * as api from '../lib/api';
+
+// 액션 타입을 선언합니다.
+// 한 요청당 세 개를 만들어야 합니다.
+
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+
+// thunk 함수를 생성합니다.
+// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치합니다.
+
+export const getPost = id => async dispatch => {
+    dispatch({ type: GET_POST });
+    try {
+        const response = await api.getPost(id);
+        dispatch({
+            type: GET_POST_SUCCESS,
+            payload: response.data
+        }); // 요청 성공
+    } catch (e) {
+        dispatch({
+            type: GET_POST_FAILURE,
+            payload: response.data,
+            error: true
+        }); // 에러 발생
+        throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
+    }
+};
+
+// 초기 상태를 선언합니다.
+// 요청의 로딩 중 상태는 loading이라는 객체에서 관리합니다.
+
+const initialState = {
+    loading: {
+        GET_POST: false,
+        GET_USERS: false
+    },
+    post: null,
+    users: null
+};
+
+const sample = handleActions(
+    {
+        [GET_POST]: state => ({
+            ...state,
+            loading: {
+                ...state.loading,
+                GET_POST: true // 요청 시작
+            }
+        }),
+        [GET_POST_SUCCESS]: (state, action) => ({
+            ...state,
+            loading: {
+                ...state.loading,
+                GET_POST: false // 요청 완료
+            },
+            post: action.payload
+        }),
+        [GET_POST_FAILURE]: (state, action) => ({
+            ...state,
+            loading: {
+                ...state.loading,
+                GET_POST: false // 요청 완료
+            }
+        }),
+        [GET_USERS]: state => ({
+            ...state,
+            loading: {
+                ...state.loading,
+                GET_USERS: true // 요청 시작
+            }
+        }),
+        [GET_USERS_SUCCESS]: (state, action) => ({
+            ...state,
+            loading: {
+                ...state.loading,
+                GET_USERS: false // 요청 완료
+            }
+        }),
+    },
+    initialState
+);
+
+export default sample;
